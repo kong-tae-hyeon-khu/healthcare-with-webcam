106ab60cf7a129c9d1f4208f6b2ca46066b147d8
공태현 <kong3047@Khu.ac.kr>
2022-05-26 02:28:16 +0900

    Routing HTML File on server.js

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const ejs = require('ejs')
+
 const app = express();
 const port = 3000
 
+
+app.set('port', port)
+app.set('view engine', 'ejs')
+app.engine('html',ejs.renderFile )
+
+
 app.get('/', (req,res) => {
-    res.send("Hello World")
+    app.set('views', __dirname + '/views/mainPage')
+    res.render('main.html')
+})
+
+app.get('/squart', (req,res) => {
+    app.set('views', __dirname + '/views/squartPage')
+    res.render('squart.html')
 })
 
 app.listen(port, () => {
